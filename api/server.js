3c8e5944cd57f7c3da9b45fda058dc01f8116148
Raj-04 <1204patel.raj@gmail.com>
2021-11-29 18:14:38 -0500

    can get by ID

--- api/server.js
+++ api/server.js
@@ -13,8 +13,22 @@ server.get('/api/users', async (req, res) => {
     try {
         const users = await Users.find()
         res.status(200).json(users)
-    } catch (err) {
+    } catch(err) {
         res.status(500).json({ message: "The users information could not be retrieved" })
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await Users.findById(id)
+        if (!user) {
+            res.status(404).json({ message: 'The user with the specified ID does not exist'})
+        } else {
+            res.status(200).json(user)
+        }
+    } catch (error) {
+        res.status(500).json({ errormessage: 'The user information could not be retrieved'})
     }
 })
 
