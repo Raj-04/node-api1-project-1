0533c987f5f0d7f544340a3b25165a0b573cecb3
Raj-04 <1204patel.raj@gmail.com>
2021-11-29 18:17:38 -0500

    all tests passing

--- api/server.js
+++ api/server.js
@@ -61,4 +61,21 @@ server.delete('/api/users/:id', async (req, res) => {
     })
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        const updatedUser = await Users.update(id, {name, bio})
+        if (!updatedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else if (!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user." })
+        } else {
+            res.status(200).json(updatedUser)
+        }
+    } catch(error) {
+        res.status(500).json({ message: "The user could not be removed" })
+    }
+})
+
 module.exports = server
